f4105f640659ad35fbd54f264e904f7223235689
Iván Sierra <ivan.sierra.perez@colegiolitterator.com>
2024-10-26 02:34:49 +0200

    tablero dinamico

--- main.py
+++ main.py
@@ -4,42 +4,45 @@ import random
 
 Nombre = input("Introduce tu nombre: ")
 
-filas = 0
-columnas =0
-
-fila = int(input("Dime cuantas filas quieres que tenga el tablero"))
-columnas = int(input("Dime cuantas columnas quiere que tenga el tablero"))
-while((filas * columnas) % 2 != 0):
-    print("La multiplicacion de filas x columnas tiene que dar par")
-    fila = int(input("Dime cuantas filas quieres que tenga el tablero"))
-    columnas = int(input("Dime cuantas columnas quiere que tenga el tablero"))
+# Solicitamos las dimensiones del tablero
+filas = int(input("Dime cuántas filas quieres que tenga el tablero: "))
+columnas = int(input("Dime cuántas columnas quieres que tenga el tablero: "))
 
 
+# Verificamos que la multiplicación de filas y columnas sea par y no sea mayor a 5 x 6 
+while (filas * columnas) % 2 != 0 or filas > 5 or columnas > 6:
+    if filas > 6 or columnas > 6:
+        print("Lo máximo permitido para el tablero es 5 x 6")
+    else:
+        print("La multiplicación de filas x columnas tiene que dar par")
+    
+    filas = int(input("Dime cuántas filas quieres que tenga el tablero: "))
+    columnas = int(input("Dime cuántas columnas quieres que tenga el tablero: "))
 
 
 
-# Rellenamos la tabla con nuemeros aleatorios del 1 al 15
+
+
 lista = []
-for i in range(1, 16):
+num_pares = (filas * columnas) // 2  # La cantidad de pares necesarios
+for i in range(1, num_pares + 1):
     lista.extend([i, i])
 
 # Desordenamos la lista
 random.shuffle(lista)
 
-tabla = [lista[i:i+columnas] for i in range(0, len(lista), columnas)]
-
-listaOculta = lista
-
+listaOculta = lista.copy()
 
 cont = 0
 for i in range(len(listaOculta)):
     # Imprime tablero oculto
     print("-", end=" ") 
     cont += 1
-    if cont == 5:
+    if cont == columnas:
         cont = 0
         print()
-        
+
+print()   
 for i in range(len(lista)):
     # Verificamos la longitud del número en `lista[i]` para agregar los espacios necesarios
     if lista[i] < 10:
@@ -47,7 +50,7 @@ for i in range(len(lista)):
     else:
         print(" ", lista[i], end=" ")
     cont += 1
-    if cont == 5:
+    if cont == columnas:
         cont = 0
         print()
 
